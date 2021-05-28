98c7fe95bd624f8862782f14dcc550422c155fad
Sal Ferrarello <sal@ironcodestudio.com>
2021-05-28 16:32:50 -0400

    Initial - load users on login page

---
+++ wp-fast-login.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace salcode\fastLogin;
+
+add_action( 'login_form', __NAMESPACE__ . '\login_form' );
+
+function login_form() {
+	$args = [
+		'number' => 100,
+		'role' => 'Administrator',
+	];
+	$user_query = new \WP_User_Query( $args );
+?>
+<div>
+	<label for="fast-login">Choose account to fast login</label><br>
+	<select
+		id="fast-login"
+	>
+		<?php
+			printUserOptionTags( [
+				'number' => 100,
+				'role' => 'Administrator'
+			]);
+
+			printUserOptionTags( [
+				'number' => 100,
+				'role__not_in' => [ 'Administrator' ]
+			]);
+		?>
+	</select>
+	<br><br>
+</div>
+<?php
+}
+
+function printUserOptionTags( $args ) {
+	$user_query = new \WP_User_Query( $args );
+	foreach ( $user_query->get_results() as $user ) {
+		printf(
+			'<option>%s</option>',
+			$user->display_name
+		);
+	}
+}
