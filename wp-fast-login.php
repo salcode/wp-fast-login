46f17ef97af2ebe74ba75644ca99ae4e8cc66fb1
Sal Ferrarello <sal@ironcodestudio.com>
2021-05-29 11:45:43 -0400

    Add REST API route
    Add REST API route for POST to /wp-fast-login/v1/login/{userId}
    
    Hitting this REST API route will set a cookie to make the browser logged
    in as $userId

--- wp-fast-login.php
+++ wp-fast-login.php
@@ -4,6 +4,19 @@ namespace salcode\fastLogin;
 
 add_action( 'login_form', __NAMESPACE__ . '\login_form' );
 add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\enqueue_scripts' );
+add_action( 'rest_api_init', __NAMESPACE__ . '\add_rest_api_route');
+
+function add_rest_api_route() {
+	register_rest_route( 'wp-fast-login/v1', '/login/(?P<id>[\d]+)', [
+		'callback' => function( $request ) {
+			$user_id = $request['id'];
+			wp_set_auth_cookie( $user_id, TRUE );
+			return rest_ensure_response([ 'userId' => $user_id ]);
+		},
+		'permission_callback' => '__return_true',
+		'methods'  => \WP_REST_Server::CREATABLE, // POST
+	]);
+}
 
 function enqueue_scripts() {
 	wp_enqueue_script(
