14686c5f28149376af0a9b91882dab4bdb6225b0
Sal Ferrarello <sal@ironcodestudio.com>
2021-05-29 11:59:43 -0400

    Add early return if not userId
    If no userId is selected (e.g. the "Select a User" option in the
    <select> was chosen), return early (do not make the call to the REST API
    endpoint)

--- js/wp-fast-login.js
+++ js/wp-fast-login.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   document.getElementById('fast-login').addEventListener(
     'input', async function(evt) {
+      if (! this.value) {
+        // No user selected. Return early with no changes.
+         return;
+      }
       try {
         const userId = await fetch(
           `${wpFastLogin.restUrl}wp-fast-login/v1/login/${this.value}`,
