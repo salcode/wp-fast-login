759397544143b55637df12bc9b7e9edf45398f73
Sal Ferrarello <sal@ironcodestudio.com>
2021-05-29 11:47:29 -0400

    Add JS to call REST API endpoint and redirect
    Add JS to call REST API endpoint and redirect to the destination URL

--- js/wp-fast-login.js
+++ js/wp-fast-login.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   document.getElementById('fast-login').addEventListener(
-    'input', function(evt) {
-      console.log(this.value);
+    'input', async function(evt) {
+      try {
+        const userId = await fetch(
+          `${wpFastLogin.restUrl}wp-fast-login/v1/login/${this.value}`,
+          { method: 'POST' }
+        ).then((response) => response.json())
+        .then((json) => json.userId);
+
+        if (! userId) {
+          throw new Error( 'Attempt to login failed' );
+        }
+        window.location.href = wpFastLogin.destination;
+      } catch (err) {
+        alert(err);
+      }
     }
   );
 });
